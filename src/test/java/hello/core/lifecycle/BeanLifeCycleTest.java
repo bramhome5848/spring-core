d27bdd948c9e8f1ab3576f7a0556d7b2b64c8996
이경진 <bramhome5848@gmail.com>
2020-10-02 15:59:54 +0900

    Bean Life Cycle Callback

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -11,13 +11,13 @@ class BeanLifeCycleTest {
     public void lifeCycleTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
-        ac.close();
+        ac.close(); //컨테이너 종료
     }
 
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        @Bean /*(initMethod = "init", destroyMethod = "close")*/
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
